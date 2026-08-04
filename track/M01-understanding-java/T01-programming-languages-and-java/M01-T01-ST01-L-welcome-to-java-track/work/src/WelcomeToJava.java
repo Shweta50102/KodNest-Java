8b6ce02e1c12f3b6de1ef194d02efa64b58dfb7d
Shweta <shwetamudakude5012002@gmail.com>
2026-08-04 12:26:54 +0530

    Added some lines

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome To Java Track");
+        System.out.println("Welcome To Java Track");
+        System.out.println("My name is Shweta");
+        System.out.println("My goal is to became Java Developer.");
+        System.out.println("I selected java because I want to build large applications.");
+
     }
 }
