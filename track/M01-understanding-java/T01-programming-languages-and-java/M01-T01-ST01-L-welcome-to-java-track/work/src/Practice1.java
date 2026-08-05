53dbc958229c580ab23d21cec6099e8a9d94790e
Shweta <shwetamudakude5012002@gmail.com>
2026-08-05 12:32:29 +0530

    Practice1

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Practice1.java
@@ -0,0 +1,11 @@
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        // Display the student's learning track.
+        System.out.println("Track: Java Development");
+
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome To Java Track");
+        System.out.println("My name is Shweta");
+        System.out.println("My goal is to became Java Developer.");
+        System.out.println("I selected java because I want to build large applications.");
     }
 }
