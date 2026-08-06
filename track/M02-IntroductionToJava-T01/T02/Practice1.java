fb7e149c9b55517c431a6c28978ab96a4543b415
Shweta <shwetamudakude5012002@gmail.com>
2026-08-06 15:32:22 +0530

    push

---
+++ track/M02-IntroductionToJava-T01/T02/Practice1.java
@@ -0,0 +1,30 @@
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        int number = -7;
+        int firstScore = 18;
+        int secondScore = 25;
+
+        if (number > 0) {
+            System.out.println("Number Type: Positive");
+        } else if (number < 0) {
+            System.out.println("Number Type: Negative");
+        } else {
+            System.out.println("Number Type: Zero");
+        }
+
+        if (number % 2 == 0) {
+            System.out.println("Parity: Even");
+        } else {
+            System.out.println("Parity: Odd");
+        }
+
+        if (firstScore >= secondScore) {
+            System.out.println("Larger Score: " + firstScore);
+        } else {
+            System.out.println("Larger Score: " + secondScore);
+        }
+
+    }
+}
